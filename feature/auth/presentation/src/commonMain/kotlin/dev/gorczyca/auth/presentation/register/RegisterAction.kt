b107629d5743e3a90f8ec24de5fa0234d64ed43a
rgorczyca <gorczyca.it@gmail.com>
2026-05-20 00:03:32 +0200

    Validate Register Form Fields

--- feature/auth/presentation/src/commonMain/kotlin/dev/gorczyca/auth/presentation/register/RegisterAction.kt
+++ feature/auth/presentation/src/commonMain/kotlin/dev/gorczyca/auth/presentation/register/RegisterAction.kt
@@ -2,7 +2,6 @@ package dev.gorczyca.auth.presentation.register
 
 sealed interface RegisterAction {
     data object OnLoginClick: RegisterAction
-    data object OnInputTextFocusGain: RegisterAction
     data object OnRegisterClick: RegisterAction
     data object OnTogglePasswordVisibilityClick: RegisterAction
 }
