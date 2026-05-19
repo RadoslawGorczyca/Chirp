0d00d4fe1a0fe55e323f71624df8b1276f0d4382
rgorczyca <gorczyca.it@gmail.com>
2026-05-20 00:06:08 +0200

    Revert deleting OnInputTextFocusGain RegisterAction

--- feature/auth/presentation/src/commonMain/kotlin/dev/gorczyca/auth/presentation/register/RegisterAction.kt
+++ feature/auth/presentation/src/commonMain/kotlin/dev/gorczyca/auth/presentation/register/RegisterAction.kt
@@ -2,6 +2,7 @@ package dev.gorczyca.auth.presentation.register
 
 sealed interface RegisterAction {
     data object OnLoginClick: RegisterAction
+    data object OnInputTextFocusGain: RegisterAction
     data object OnRegisterClick: RegisterAction
     data object OnTogglePasswordVisibilityClick: RegisterAction
 }
